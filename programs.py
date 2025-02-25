156e36f4eba5ffdfddeee6d7404962862fd8aafb
Klimpen <axco2004@gmail.com>
2025-02-26 01:15:49 +1300

    cleaned up programs

--- programs.py
+++ programs.py
@@ -2,4 +2,4 @@
 class Programs:
 
     def fib_gen():
-        return [2, 2, "MOV ACC IMM\n 1\n ADD ACC BAK\n ADD BAK ACC\n MOV PC IMM\n 1\n", "ADD ACC IMM\n 1\n MOV PC IMM\n -1", "MOV PC IMM\n 0", "MOV PC IMM\n 0"]
+        return [1, 1, "MOV ACC IMM\n 1\n ADD ACC BAK\n ADD BAK ACC\n MOV PC IMM\n 1\n"]
